5d9fd04a71ef87b323ce7334b49bb8b2ddbbc0d2
kimhw <kimhw@arainhr.com>
2023-05-23 17:51:19 +0900

    add

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
-createApp(App).use(router).mount('#app')
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+createApp(App).use(BootstrapVue3).use(router).mount('#app')
 
 axios.defaults.baseURL = 'http://192.168.0.62:8080/'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
